b2f314b7db9a0dd780bc409f6d8ccc312b70e23a
shrutih-cuelogic <shruti.hiremath@cuelogic.co.in>
2017-05-05 15:20:31 +0530

    removed unnecassary code

--- angularapp/angularapp.js
+++ angularapp/angularapp.js
@@ -9,22 +9,4 @@
     ]).config(['$urlRouterProvider', function($urlRouterProvider) {
         $urlRouterProvider.otherwise('/login');
     }])
-    // .run(['$rootScope', '$state', '$location', 'serverLocalStorageService', 
-    // 	function($rootScope, $state,  $location, serverLocalStorageService) {
-	   //  	$rootScope.$on('$stateChangeStart', function(event, next, current){
-	   //  		// console.log(serverLocalStorageService.get('keyid'));
-	   //  		// if (serverLocalStorageService.get('tokenid') === null || serverLocalStorageService.get('tokenid') === undefined) {
-	   //  		// 	// event.preventDefault();
-	   //  		// 	$state.go('login');
-	   //  		// } 
-	   //  		// else {
-	   //  		// 	$state.go('profile');
-	   //  		// }
-    //             if (serverLocalStorageService.get('tokenid') == null || serverLocalStorageService.get('tokenid') == undefined) {
-    //                 $state.go('login');
-    //             } else if (next.originalPath == "/login" && serverLocalStorageService.get('tokenid') != null && serverLocalStorageService.get('tokenid') != undefined) {
-    //                 $state.go('profile');
-    //             }
-	   //  	});
-	   //  }]);
 })();
